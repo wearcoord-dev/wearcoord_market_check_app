91e3f49bb710cc9605f2a21f75ccab5f8248ad81
taku matsunaga <skyline199306@yahoo.co.jp>
2021-05-22 13:46:02 +0900

    fix database.php

--- app/Library/Database.php
+++ app/Library/Database.php
@@ -35,6 +35,9 @@ class Database
                                                 $url = $i->orange;
                                                 if (!$url) {
                                                     $url = $i->purple;
+                                                    if(!$url){
+                                                        $url = $i->gray;
+                                                    }
                                                 }
                                             }
                                         }
@@ -64,6 +67,47 @@ class Database
             $DBitems = [];
             foreach ($item as $i) {
                 $url = $i->$color;
+                $DBitems[] = array('db' => $i, 'url' => $url);
+            }
+        }else if($category){
+            $item = DB::table( $type . '_rakuten_apis')->where('category', $category)->get();
+
+            // urlに画像を入れる
+            $DBitems = [];
+            foreach ($item as $i) {
+                
+                $url = $i->black;
+
+                if (!$url) {
+                    $url = $i->white;
+                    if (!$url) {
+                        $url = $i->blue;
+                        if (!$url) {
+                            $url = $i->red;
+                            if (!$url) {
+                                $url = $i->green;
+                                if (!$url) {
+                                    $url = $i->yellow;
+                                    if (!$url) {
+                                        $url = $i->navy;
+                                        if (!$url) {
+                                            $url = $i->pink;
+                                            if (!$url) {
+                                                $url = $i->orange;
+                                                if (!$url) {
+                                                    $url = $i->purple;
+                                                    if(!$url){
+                                                        $url = $i->gray;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
                 $DBitems[] = array('db' => $i, 'url' => $url);
             }
         }
