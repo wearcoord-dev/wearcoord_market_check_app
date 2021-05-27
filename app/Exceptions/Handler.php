b073a07663c3f335a70074efa4f15e72277ce137
taku matsunaga <skyline199306@yahoo.co.jp>
2021-05-27 22:58:05 +0900

    redirect https

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -37,5 +38,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+
+        if ($exception instanceof TokenMismatchException) {
+            return redirect('/login');
+        }
+
+        return parent::render($request, $exception);
     }
 }
