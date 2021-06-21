395bd8676a18266c6508e5b6304b4daf3505d573
taku matsunaga <skyline199306@yahoo.co.jp>
2021-06-21 14:56:26 +0900

    create select only one category male

--- resources/js/components/molecules/searchbox/male/SearchCategoryPants.jsx
+++ resources/js/components/molecules/searchbox/male/SearchCategoryPants.jsx
@@ -1,22 +1,37 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 
 export const SearchCategoryPants = memo((props) => {
     const { setValueCategory } = props;
+    const [check, setCheck] = useState(1);
 
     // radioのonChangeイベント発生時
     const handleInputChange = (props) => {
         const category = props.target.value;
 
+        // 最初のinput以外にチェックが入ったかどうか確認
+        if (props.target.value !== "508772") {
+            setCheck(0);
+        } else {
+            setCheck(1);
+        }
+
         setValueCategory(category);
     }
+
+    // 最初はhalfにセット
+    useEffect(() => {
+        const category = "508772";
+        setValueCategory(category);
+    }, []);
 
 
     return (
         <>
-            {/* <p className="searchTitle">Category : </p> */}
             <div className="slide_x">
 
-                <input type="radio" id="half" className="radioInput" name="category" value="508772" onChange={handleInputChange} />
+                <input type="radio" id="half" className="radioInput" name="category" value="508772" onChange={handleInputChange}
+                checked={check}
+                 />
                 <label htmlFor="half" className="radioInputLabel">ハーフ・ショート</label>
 
                 <input type="radio" id="long" className="radioInput" name="category" value="565926" onChange={handleInputChange} />
