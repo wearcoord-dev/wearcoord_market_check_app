1207523e81ad377ccdbc701a28977de5fafacdb6
taku matsunaga <skyline199306@yahoo.co.jp>
2021-06-24 21:27:37 +0900

    create item search shoes

---
+++ resources/js/components/organisms/layouts/item/search/ItemSearchShoes.jsx
@@ -0,0 +1,57 @@
+import { memo, useContext, useState } from "react";
+import { SearchBrandCaps } from "../../../../molecules/searchbox/searchBrand/SearchBrandCaps";
+import { SearchColor } from "../../../../molecules/searchbox/SearchColor";
+import { UserContext } from "../../../../providers/UserProvider";
+import SearchIcon from '@material-ui/icons/Search';
+import { Button } from "@material-ui/core";
+import { SearchCategoryTops } from "../../../../molecules/searchbox/male/SearchCategoryTops";
+import { SearchCategoryTopsFemale } from "../../../../molecules/searchbox/female/SearchCategoryTopsFemale";
+import { SearchCategoryPants } from "../../../../molecules/searchbox/male/SearchCategoryPants";
+import { SearchCategoryPantsFemale } from "../../../../molecules/searchbox/female/SearchCategoryPantsFemale";
+
+
+
+export const ItemSearchShoes = memo((props) => {
+    const { onClickFetchShoes } = props;
+    const [value, setValue] = useState("");
+    const [valueColor, setValueColor] = useState("");
+    const [valueCategory, setValueCategory] = useState("");
+
+    const context = useContext(UserContext);
+
+    const handleClick = () => {
+        let category = "";
+
+        if(context.contextName.gender == 'male'){
+            category = "208025";
+        }else{
+            category = "565819";
+        }
+
+        const data = {
+            wear: "shoes",
+            brand: value,
+            color: valueColor,
+            category: category,
+        }
+
+        // console.log(data);
+
+        onClickFetchShoes(data);
+    }
+
+    return (
+        <>
+            <div>
+                <SearchBrandCaps setValue={setValue}
+                />
+                <SearchColor setValueColor={setValueColor} />
+
+                <Button style={{ position: "fixed", bottom: "-36px", left: "50%", transform: "translateX(-50%)", width: "250px" }} variant="contained" color="primary" onClick={handleClick}>
+                    <SearchIcon style={{ paddingRight: "6px" }} />
+                    検索する
+                </Button>
+            </div>
+        </>
+    )
+})
