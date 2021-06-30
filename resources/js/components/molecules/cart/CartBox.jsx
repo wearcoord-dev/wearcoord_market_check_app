159155c31ee3cbea5815beaacddbb3fccda70203
taku matsunaga <skyline199306@yahoo.co.jp>
2021-06-30 16:22:51 +0900

    add delete front

--- resources/js/components/molecules/cart/CartBox.jsx
+++ resources/js/components/molecules/cart/CartBox.jsx
@@ -1,12 +1,12 @@
-import { Card, IconButton } from "@material-ui/core";
-import { memo, useContext, useEffect } from "react";
+import { Backdrop, Button, Card, Fade, IconButton, Modal } from "@material-ui/core";
+import { memo, useContext, useEffect, useState } from "react";
 import { useGetCartItem } from "../../../hooks/cart/useGetCartItem";
 import { UserContext } from "../../providers/UserProvider";
 import { makeStyles } from '@material-ui/core/styles';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         minWidth: 275,
         margin: "10px auto",
@@ -14,14 +14,50 @@ const useStyles = makeStyles({
     },
     cardwrap: {
         margin: "70px 0",
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+    btn: {
+        marginTop: '20px',
+        width: '100%',
+        padding: '10px 0',
+        maxWidth: '300px',
+        left: "50%",
+        transform: "translateX(-50%)"
     }
-});
+}));
 
 export const CartBox = memo((props) => {
     const { type } = props;
     const { GetCartItem, userItemCartInfo } = useGetCartItem();
     const context = useContext(UserContext);
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [getid, setGetid] = useState('');
+    const [getHtml, setGetHtml] = useState('');
+
+    const handleOpen = (id, html) => {
+        setOpen(true);
+        setGetid(id);
+        setGetHtml(html);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const onClickDelete = () => {
+        console.log('delete');
+    }
 
     useEffect(() => {
         if (context.contextName) {
@@ -42,7 +78,7 @@ export const CartBox = memo((props) => {
                                 <Card className={classes.root}>
                                     <div style={{ display: "flex", justifyContent: "center" }} dangerouslySetInnerHTML={{ __html: item.moshimoLink }}></div>
                                 </Card>
-                                <IconButton color="secondary" aria-label="add an delete">
+                                <IconButton color="secondary" aria-label="add an delete" onClick={handleOpen.bind(this, item.id, item.moshimoLink)}>
                                     <DeleteForeverIcon fontSize="large" />
                                 </IconButton>
                             </div>
@@ -50,6 +86,30 @@ export const CartBox = memo((props) => {
                     </>
                 ) : <p>ありません</p>}
             </div>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">このアイテムをカートから削除しますか？</h2>
+                        <div style={{ display: "flex", justifyContent: "center" }} dangerouslySetInnerHTML={{ __html: getHtml }}></div>
+                        <Button className={classes.btn} variant="contained" color="primary" onClick={onClickDelete}>
+                            <DeleteForeverIcon style={{ paddingRight: "6px" }} />
+                            削除する
+                        </Button>
+                    </div>
+                </Fade>
+            </Modal>
         </>
     )
 })
