1da4adc62eb31d943428bb338b5e9fb2d76b3d98
taku matsunaga <skyline199306@yahoo.co.jp>
2021-08-08 19:48:52 +0900

    fix search btn

--- resources/js/components/molecules/searchbox/WearSearch.jsx
+++ resources/js/components/molecules/searchbox/WearSearch.jsx
@@ -53,14 +53,33 @@ const useStyles = makeStyles({
         // width: "90%"
     },
     closeBtn: {
-        width: "40px",
-        height: "40px",
-        backgroundColor: "#216496",
-        borderRadius: "50%",
+        // width: "40px",
+        // height: "40px",
+        // backgroundColor: "#216496",
+        // borderRadius: "50%",
+        // position: "absolute",
+        // top: "-30px",
+        // right: "10px",
+        // fontSize: "10px",
+        top: " 0px",
+        right: "0px",
+        color: "#fff",
+        width: "50px",
+        cursor: "pointer",
+        // padding: "10px 0",
         position: "absolute",
-        top: "-30px",
-        right: "10px",
-        fontSize: "10px",
+        boxSizing: "border-box",
+        // lineHeight: "50px",
+        borderRadius: "6px 0 0 6px",
+        letterSpacing: "0.1em",
+        backgroundColor: "#216496",
+        height: "49px",
+        // writingMode: "vertical-rl",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: "100",
     },
     register: {
         position: "absolute",
@@ -68,16 +87,26 @@ const useStyles = makeStyles({
         backgroundColor: "#216496",
         color: "#fff",
         // height: "30px",
-        top: "-130px",
+        top: "0px",
         left: "0px",
         borderRadius: "0 6px 6px 0",
-        writingMode: "vertical-rl",
-        padding: "10px 0",
+        // writingMode: "vertical-rl",
+        // padding: "10px 0",
         letterSpacing: "0.1em",
         cursor: "pointer",
         boxSizing: "border-box",
         width: "50px",
-        lineHeight: "50px",
+        // lineHeight: "50px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "49px",
+        zIndex: "100",
+    },
+    buttonText: {
+        // height: "100%",
+        // textAlign: "center",
     }
 });
 
@@ -94,8 +123,8 @@ export const WearSearch = memo((props) => {
         <>
             <Container>
                 <Paper className={classes.root}>
-                    <button className={classes.closeBtn} type="button" onClick={handleClick} ><CloseIcon style={{ color: "white" }} /></button>
-                    <div className={classes.register} type="button" onClick={onClickRegisterWear} ><CheckCircleOutlineIcon style={{ paddingBottom: "6px" }} />ウェアを確定</div>
+                    <div className={classes.closeBtn} type="button" onClick={handleClick} ><CloseIcon style={{ color: "white", paddingBottom: "6px" }} /><p className={classes.buttonText}>閉じる</p></div>
+                    <div className={classes.register} type="button" onClick={onClickRegisterWear} ><CheckCircleOutlineIcon style={{ paddingBottom: "6px" }} />確定</div>
                     <Tabs
                         value={value}
                         onChange={handleChange}
