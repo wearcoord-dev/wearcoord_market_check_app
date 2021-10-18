f5673ed4730a9e6e1bcb301b087cd040e10ad1b3
taku matsunaga <skyline199306@yahoo.co.jp>
2021-10-18 20:00:56 +0900

    delete brand

--- resources/js/components/molecules/searchbox/searchBrand/SearchBrandTops.jsx
+++ resources/js/components/molecules/searchbox/searchBrand/SearchBrandTops.jsx
@@ -189,11 +189,11 @@ export const SearchBrandTops = memo((props) => {
                  />
                 <label htmlFor="lacoste" className="radioInputLabel">Lacoste</label>
 
-                <input type="radio" id="yuuchan" className="radioInput" name="brand" value="yuuchan" onChange={handleInputChange}
+                {/* <input type="radio" id="yuuchan" className="radioInput" name="brand" value="yuuchan" onChange={handleInputChange}
                 onClick={onClickInput}
                 checked={check.yuuchan}
                  />
-                <label htmlFor="yuuchan" className="radioInputLabel">yuuchan</label>
+                <label htmlFor="yuuchan" className="radioInputLabel">yuuchan</label> */}
 
                 <input type="radio" id="ralosso" className="radioInput" name="brand" value="ralosso" onChange={handleInputChange}
                 onClick={onClickInput}
