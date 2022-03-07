cad884a467a3e9769974b2dae7a43c82afdedd6b
Taku Matsunaga <corvettez06511@gmail.com>
2022-03-07 17:16:03 +0900

    get shopify item test

--- resources/js/components/notLoginComponents/atoms/drawer/TopDrawerBtn.tsx
+++ resources/js/components/notLoginComponents/atoms/drawer/TopDrawerBtn.tsx
@@ -1,5 +1,6 @@
-import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Icon, useDisclosure } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Icon, useDisclosure } from "@chakra-ui/react";
+import { FC, memo, useEffect, useState } from "react";
+import { useGetShopifyItem } from "../../hooks/useGetShopifyItem";
 
 type Props = {
     btnIcon: any;
@@ -10,10 +11,40 @@ type Props = {
 export const TopDrawerBtn: FC<Props> = memo((props) => {
     const { btnIcon, wearId, type } = props;
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [shopifyId, setShopifyId] = useState<any>();
+    const { getShopifyItem, shopifyItem } = useGetShopifyItem();
+
+    const shopifyItemNum = 7032136138938;
 
     const handleClick = () => {
         onOpen()
+
+        if (shopifyId) {
+            getShopifyItem(shopifyId);
+        }
     }
+    // console.log(btoa("gid://shopify/Product/7032136138938"));
+    // console.log(atob("Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMzIxMzYxMzg5Mzg="));
+
+    console.log(shopifyItem)
+
+
+    useEffect(() => {
+        setShopifyId(btoa(`gid://shopify/Product/${shopifyItemNum}`));
+    }, [shopifyId])
+
+    const shopifyImg = (
+        <>
+            {shopifyItem ? (
+                <>
+                    {shopifyItem.images.edges.map((item) => {
+                        <p>{item.node.originalSrc}</p>
+                    })}
+                </>
+            ) : <p>null</p>}
+        </>
+    );
+
 
     return (
         <>
@@ -24,15 +55,40 @@ export const TopDrawerBtn: FC<Props> = memo((props) => {
                 p={8}
             ><Icon w={8} h={8} color='white' as={btnIcon} /></Button>
 
-        <Drawer onClose={onClose} isOpen={isOpen} size={'md'}>
-            <DrawerOverlay />
-            <DrawerContent>
-                <DrawerHeader>{`${type} drawer contents`}</DrawerHeader>
-                <DrawerBody>
-                    {`ここに${wearId}のアイテムを表示`}
-                </DrawerBody>
-            </DrawerContent>
-        </Drawer>
+            <Drawer onClose={onClose} isOpen={isOpen} size={'md'}>
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerHeader>{`${type} drawer contents`}</DrawerHeader>
+                    <DrawerHeader>{shopifyItem ? <p>{shopifyItem.title}</p> : null}</DrawerHeader>
+                    <DrawerHeader>{shopifyItem ? <p>{shopifyItem.vendor}</p> : null}</DrawerHeader>
+                    <DrawerBody>
+                        {shopifyItem ? (
+                            <>
+                                <div dangerouslySetInnerHTML={{ __html: shopifyItem.descriptionHtml }}></div>
+                            </>
+                        ) : <p>null</p>}
+                        {shopifyItem ? (
+                            <>
+                                {shopifyItem.variants.edges[0].node.price}
+                            </>
+                        ) : <p>null</p>}
+                        {shopifyItem ? (
+                            <>
+                                {shopifyItem.images.edges.map((item) => (
+                                    < img src={item.node.originalSrc} key={item.node.id} />
+                                ))}
+                            </>
+                        ) : <p>null</p>}
+                        {shopifyItem ? (
+                            <>
+                                    <DrawerFooter>
+                                        <Button as="a" href={shopifyItem.onlineStoreUrl} bg='#216496' color='whiteAlpha.700' variant='solid' target={'_blank'}>購入する</Button>
+                                    </DrawerFooter>
+                            </>
+                        ) : <p>null</p>}
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </>
     )
 });
---
+++ resources/js/components/notLoginComponents/hooks/useGetShopifyItem.ts
@@ -0,0 +1,67 @@
+import { useToast } from "@chakra-ui/react";
+import { useCallback, useState } from "react";
+
+type Props = {
+    shopifyId: any;
+}
+
+export const useGetShopifyItem = () => {
+    const [shopifyItem, setShopifyItem ] = useState<any>();
+
+    const getShopifyItem = useCallback((shopifyId: Props) => {
+
+        const query = `{
+  node(id: "${shopifyId}") {
+    id
+    ... on Product {
+                    title
+                    id
+                    title
+                    description
+                    descriptionHtml
+                    onlineStoreUrl
+                    createdAt
+                    totalInventory
+                    availableForSale
+                    handle
+                    vendor
+                    images(first: 5) {
+                    edges {
+                      node {
+                     id
+                           altText
+                           originalSrc
+                               }
+                              }
+                               }
+                    variants(first: 5) {
+                     edges {
+                         node {
+                          price
+                             id
+                                      }
+                                  }
+                              }
+    }
+  }
+}`;
+
+    apiCall(query).then(response => {
+        setShopifyItem(response.data.node);
+    });
+
+    }, []);
+
+    function apiCall(query) {
+        return fetch('https://wearcoord.myshopify.com/api/2020-10/graphql.json', {   //[shop-name]にurlのショップ名を入力
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/graphql',
+                'X-Shopify-Storefront-Access-Token': "118cc9ac9543b7acebdb47fee07d8e9e"   //Storefront APIのアクセストークンを入力
+            },
+            "body": query
+        }).then(response => response.json());
+    }
+
+    return { getShopifyItem, shopifyItem };
+}
