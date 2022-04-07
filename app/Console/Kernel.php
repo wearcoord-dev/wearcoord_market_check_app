3805b11974e8f74e93c08fc135bb0833e03e23cb
Taku Matsunaga <corvettez06511@gmail.com>
2022-04-07 13:40:56 +0900

    add task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,12 +26,30 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        // 毎日深夜0時に実行
         $schedule->call(function () {
             DB::table('caps_counts')->update(['day' => 0]);
             DB::table('tops_counts')->update(['day' => 0]);
             DB::table('pants_counts')->update(['day' => 0]);
             DB::table('shoes_counts')->update(['day' => 0]);
-        })->everyMinute();
+        })->daily();
+
+        // 毎週日曜日の00:00にタスク実行
+        $schedule->call(function () {
+            DB::table('caps_counts')->update(['week' => 0]);
+            DB::table('tops_counts')->update(['week' => 0]);
+            DB::table('pants_counts')->update(['week' => 0]);
+            DB::table('shoes_counts')->update(['week' => 0]);
+        })->weekly();
+
+        // 毎月１日の00:00にタスク実行
+        $schedule->call(function () {
+            DB::table('caps_counts')->update(['month' => 0]);
+            DB::table('tops_counts')->update(['month' => 0]);
+            DB::table('pants_counts')->update(['month' => 0]);
+            DB::table('shoes_counts')->update(['month' => 0]);
+        })->monthly();
     }
 
     /**
