928daccca85d48bd1feddd7868a45c470aea0b42
Taku Matsunaga <corvettez06511@gmail.com>
2022-04-26 14:04:46 +0900

    base pants fixed

--- resources/js/components/notLoginComponents/pages/SampleTop.tsx
+++ resources/js/components/notLoginComponents/pages/SampleTop.tsx
@@ -1,4 +1,3 @@
-import { Button, Stack } from "@chakra-ui/react";
 import { FC, memo, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import { OpenBtn } from "../atoms/button/OpenBtn";
@@ -12,14 +11,6 @@ export const SampleTop: FC = memo(() => {
     return (
         <>
             <TopCoord />
-
-            {/* <Stack direction='row' spacing={4} align='center'>
-                <OpenBtn />
-
-                <Button onClick={onClickChangeMannequin} background='#216496' color='white' variant='solid'>
-                    マネキンを変更する
-                </Button>
-            </Stack> */}
         </>
     )
 });
